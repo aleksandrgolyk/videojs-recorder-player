e922cd244a65f694832b95566430adf14b33de46
aleksandrgolyk <aleksandr.golyk@gmail.com>
2021-09-14 15:34:13 +0300

    restored experiment branch after refactoring

--- src/domains/DeviceSelection/components/PictureInPicture/PictureInPicture.js
+++ src/domains/DeviceSelection/components/PictureInPicture/PictureInPicture.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { StyledPiPButton } from './PictureInPicture.styles'
 
-const PictureInPicture = ({ togglePip, pipEnabled }) => {
+const PictureInPicture = ({ player }) => {
+  const [pipEnabled, setPipEnabled] = useState(false)
+
+  const togglePip = async () => {
+    if (Object?.keys(player)?.length > 0) {
+      if (
+        player?.record()?.mediaElement !== document?.pictureInPictureElement
+      ) {
+        await player?.record()?.mediaElement?.requestPictureInPicture()
+        setPipEnabled(true)
+      } else {
+        await document?.exitPictureInPicture()
+        setPipEnabled(false)
+      }
+    }
+  }
   return (
     <StyledPiPButton onClick={togglePip}>
       {pipEnabled ? (
